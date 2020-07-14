9172b5dfc13edc147ca01674b9608670bccc4ca2
youshiwei <yousws@126.com>
2020-07-14 11:58:20 +0800

    下午继续

---
+++ src/api/order.js
@@ -0,0 +1,10 @@
+/**
+ * 订单管理 ajax函数
+ * created by youshiwei on Jul 14, 2020
+ */
+import req from "@/utils/request"
+
+/* 获取订单列表 */
+export function getOrderList(params) {
+    return req.get("/order/list", params)
+}
--- src/views/Order/OrderList.vue
+++ src/views/Order/OrderList.vue
@@ -1,31 +1,32 @@
 <template>
   <div class="order-list">
     <!-- 查询表单 -->
-    <el-form style="padding:20px;" :inline="true" :model="formInline" class="demo-form-inline">
+    <el-form style="padding:20px;" :inline="true" :model="searchForm" class="demo-form-inline">
       <!-- 订单号 -->
       <el-form-item label="订单号">
-        <el-input v-model="formInline.orderNumber" placeholder="订单号"></el-input>
+        <el-input v-model="searchForm.orderNo" placeholder="订单号"></el-input>
       </el-form-item>
       <!-- 收货人 -->
       <el-form-item label="收货人">
-        <el-input v-model="formInline.orderNumber" placeholder="收货人"></el-input>
+        <el-input v-model="searchForm.consignee" placeholder="收货人"></el-input>
       </el-form-item>
       <!-- 手机号 -->
       <el-form-item label="手机号">
-        <el-input v-model="formInline.phone" placeholder="手机号"></el-input>
+        <el-input v-model="searchForm.phone" placeholder="手机号"></el-input>
       </el-form-item>
       <!-- 订单状态 -->
       <el-form-item label="订单状态">
-        <el-select v-model="formInline.orderStatus" placeholder="订单状态">
+        <el-select v-model="searchForm.orderState" placeholder="订单状态">
           <el-option value="已受理">已受理</el-option>
+          <el-option value="派送中">派送中</el-option>
+          <el-option value="已完成">已完成</el-option>
         </el-select>
       </el-form-item>
-      <!-- 选择时间 -->
-      <el-form-item label="选择时间">
+      <!-- 下单时间 -->
+      <el-form-item label="下单时间">
         <el-date-picker
-          v-model="value2"
+          v-model="searchForm.date"
           type="datetimerange"
-          :picker-options="pickerOptions"
           range-separator="至"
           start-placeholder="开始日期"
           end-placeholder="结束日期"
@@ -34,29 +35,29 @@
       </el-form-item>
       <!-- 查询按钮 -->
       <el-form-item>
-        <el-button type="primary" @click="onSubmit">查询</el-button>
+        <el-button type="primary" @click="search">查询</el-button>
       </el-form-item>
     </el-form>
     <!-- 表格 -->
     <el-table :data="tableData" border style="width:100%">
       <!-- 订单号 -->
-      <el-table-column fixed prop="orderNumber" label="订单号" width="150"></el-table-column>
+      <el-table-column fixed prop="orderNo" label="订单号" width="150"></el-table-column>
       <!-- 下单时间 -->
-      <el-table-column prop="orderTime" label="下单时间" width="120"></el-table-column>
+      <el-table-column prop="orderTime" label="下单时间" width="160"></el-table-column>
       <!-- 手机号 -->
       <el-table-column prop="phone" label="手机号" width="120"></el-table-column>
       <!-- 收货人 -->
-      <el-table-column prop="receiver" label="收货人" width="120"></el-table-column>
+      <el-table-column prop="consignee" label="收货人" width="120"></el-table-column>
       <!-- 配送地址 -->
-      <el-table-column prop="address" label="配送地址" width="300"></el-table-column>
+      <el-table-column prop="deliverAddress" label="配送地址" width="200"></el-table-column>
       <!-- 送达时间 -->
-      <el-table-column prop="deliveryTime" label="送达时间" width="120"></el-table-column>
+      <el-table-column prop="deliveryTime" label="送达时间" width="160"></el-table-column>
       <!-- 用户备注 -->
-      <el-table-column prop="userSpare" label="用户备注" width="120"></el-table-column>
+      <el-table-column prop="remarks" label="用户备注" width="120"></el-table-column>
       <!-- 订单金额 -->
       <el-table-column prop="orderAmount" label="订单金额" width="120"></el-table-column>
       <!-- 订单状态 -->
-      <el-table-column prop="orderStatus" label="订单状态" width="120"></el-table-column>
+      <el-table-column prop="orderState" label="订单状态" width="120"></el-table-column>
       <el-table-column fixed="right" label="操作" width="100">
         <template slot-scope="scope">
           <el-button @click="handleClick(scope.row)" type="text" size="small">查看</el-button>
@@ -78,181 +79,55 @@
 </template>
 
 <script>
+import { getOrderList } from "@/api/order";
+import Moment from "moment";
 export default {
   data() {
     return {
-      total: 219,
+      total: 0,
       currentPage: 1,
-      pageSize: 20,
-      formInline: {
-        orderNumber: "",
-        receiver: "",
+      pageSize: 5,
+      searchForm: {
+        orderNo: "",
+        consignee: "",
         phone: "",
-        orderStatus: "",
-        period: ""
+        orderState: "",
+        date: ""
       },
-      value2: "",
-      pickerOptions: {
-        shortcuts: [
-          {
-            text: "最近一周",
-            onClick(picker) {
-              const end = new Date();
-              const start = new Date();
-              start.setTime(start.getTime() - 3600 * 1000 * 24 * 7);
-              picker.$emit("pick", [start, end]);
-            }
-          },
-          {
-            text: "最近一个月",
-            onClick(picker) {
-              const end = new Date();
-              const start = new Date();
-              start.setTime(start.getTime() - 3600 * 1000 * 24 * 30);
-              picker.$emit("pick", [start, end]);
-            }
-          },
-          {
-            text: "最近三个月",
-            onClick(picker) {
-              const end = new Date();
-              const start = new Date();
-              start.setTime(start.getTime() - 3600 * 1000 * 24 * 90);
-              picker.$emit("pick", [start, end]);
-            }
-          }
-        ]
-      },
-      tableData: [
-        {
-          orderNumber: 1424007586,
-          orderStatus: "已受理",
-          orderAmount: 49,
-          orderTime: "17:01",
-          phone: "1570****583",
-          receiver: "张一达",
-          address: "成都市高新区天华路219号天府软件园C10栋11层",
-          deliveryTime: "19:23",
-          userSpare: "微辣"
-        },
-        {
-          orderNumber: 1424007586,
-          orderStatus: "已受理",
-          orderAmount: 49,
-          orderTime: "17:01",
-          phone: "1570****583",
-          receiver: "张一达",
-          address: "成都市高新区天华路219号天府软件园C10栋11层",
-          deliveryTime: "19:23",
-          userSpare: "微辣"
-        },
-        {
-          orderNumber: 1424007586,
-          orderStatus: "已受理",
-          orderAmount: 49,
-          orderTime: "17:01",
-          phone: "1570****583",
-          receiver: "张一达",
-          address: "成都市高新区天华路219号天府软件园C10栋11层",
-          deliveryTime: "19:23",
-          userSpare: "微辣"
-        },
-        {
-          orderNumber: 1424007586,
-          orderStatus: "已受理",
-          orderAmount: 49,
-          orderTime: "17:01",
-          phone: "1570****583",
-          receiver: "张一达",
-          address: "成都市高新区天华路219号天府软件园C10栋11层",
-          deliveryTime: "19:23",
-          userSpare: "微辣"
-        },
-        {
-          orderNumber: 1424007586,
-          orderStatus: "已受理",
-          orderAmount: 49,
-          orderTime: "17:01",
-          phone: "1570****583",
-          receiver: "张一达",
-          address: "成都市高新区天华路219号天府软件园C10栋11层",
-          deliveryTime: "19:23",
-          userSpare: "微辣"
-        },
-        {
-          orderNumber: 1424007586,
-          orderStatus: "已受理",
-          orderAmount: 49,
-          orderTime: "17:01",
-          phone: "1570****583",
-          receiver: "张一达",
-          address: "成都市高新区天华路219号天府软件园C10栋11层",
-          deliveryTime: "19:23",
-          userSpare: "微辣"
-        },
-        {
-          orderNumber: 1424007586,
-          orderStatus: "已受理",
-          orderAmount: 49,
-          orderTime: "17:01",
-          phone: "1570****583",
-          receiver: "张一达",
-          address: "成都市高新区天华路219号天府软件园C10栋11层",
-          deliveryTime: "19:23",
-          userSpare: "微辣"
-        },
-        {
-          orderNumber: 1424007586,
-          orderStatus: "已受理",
-          orderAmount: 49,
-          orderTime: "17:01",
-          phone: "1570****583",
-          receiver: "张一达",
-          address: "成都市高新区天华路219号天府软件园C10栋11层",
-          deliveryTime: "19:23",
-          userSpare: "微辣"
-        },
-        {
-          orderNumber: 1424007586,
-          orderStatus: "已受理",
-          orderAmount: 49,
-          orderTime: "17:01",
-          phone: "1570****583",
-          receiver: "张一达",
-          address: "成都市高新区天华路219号天府软件园C10栋11层",
-          deliveryTime: "19:23",
-          userSpare: "微辣"
-        },
-        {
-          orderNumber: 1424007586,
-          orderStatus: "已受理",
-          orderAmount: 49,
-          orderTime: "17:01",
-          phone: "1570****583",
-          receiver: "张一达",
-          address: "成都市高新区天华路219号天府软件园C10栋11层",
-          deliveryTime: "19:23",
-          userSpare: "微辣"
-        },
-        {
-          orderNumber: 1424007586,
-          orderStatus: "已受理",
-          orderAmount: 49,
-          orderTime: "17:01",
-          phone: "1570****583",
-          receiver: "张一达",
-          address: "成都市高新区天华路219号天府软件园C10栋11层",
-          deliveryTime: "19:23",
-          userSpare: "微辣"
-        }
-      ]
+      tableData: []
     };
   },
   methods: {
+    // 获取数据列表
+    async fetchData() {
+      let { data, total } = await getOrderList({
+        currentPage: this.currentPage,
+        pageSize: this.pageSize,
+        orderNo: this.searchForm.orderNo,
+        consignee: this.searchForm.consignee,
+        phone: this.searchForm.phone,
+        orderState: this.searchForm.orderState,
+        date: this.searchForm.date
+      });
+      // 处理下单时间，送达时间
+      data.forEach(v => {
+        v.orderTime = Moment(v.orderTime).format("YYYY-MM-DD HH:mm:ss");
+        v.deliveryTime = Moment(v.deliveryTime).format("YYYY-MM-DD HH:mm:ss");
+      });
+      this.tableData = data;
+      this.total = total;
+    },
     handleClick() {},
-    onSubmit() {},
-    handleSizeChange() {},
-    handleCurrentChange() {}
+    search() {
+      this.fetchData();
+    },
+    handleCurrentChange(page) {
+      this.currentPage = page;
+      this.fetchData();
+    }
+  },
+  created() {
+    this.fetchData();
   }
 };
 </script>
